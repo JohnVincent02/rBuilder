fd2680ba3d0f157a2f52964d6466d04fe49a3832
John <j4johnvincent@gmail.com>
2026-04-23 20:33:20 +0530

    vercel file added

--- src/api/axiosinstance.js
+++ src/api/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://rbuilder-server-8a3o.onrender.com/",
   timeout: 5000,
 });
 // response interceptor
